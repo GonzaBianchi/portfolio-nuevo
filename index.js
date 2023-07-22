c009654b7b1935d938600d9daa6484d8bb47e4bc
Gonza Bianchi <gonzalobianchini70@gmail.com>
2023-07-21 21:33:00 -0300

    v.0.0.4

--- index.js
+++ index.js
@@ -8,26 +8,25 @@ const sections = document.querySelectorAll('.insta');
 
 // Función para agregar/retirar la clase "active" en la barra de navegación
 let itemActual;
-function updateNavbarActiveSection(itemActual) {
-    const windowHeight = window.innerHeight;
-  
-    sections.forEach((section, index) => {
-      const rect = section.getBoundingClientRect();
-      const sectionTop = rect.top;
-      const sectionHeight = rect.height;
-  
-      if (sectionTop <= windowHeight / 2 && sectionTop + sectionHeight >= windowHeight / 2) {
-        navbarItems.forEach((item, itemIndex) => {
-          if (itemIndex === index) {
-            item.classList.add('active');
-            itemActual = item;
-            return itemActual;
-          } else {
-            item.classList.remove('active');
-          }
-        });
-      }
-    });
+function updateNavbarActiveSection() {
+  const windowHeight = window.innerHeight;
+
+  sections.forEach((section, index) => {
+    const rect = section.getBoundingClientRect();
+    const sectionTop = rect.top;
+    const sectionHeight = rect.height;
+
+    if (sectionTop <= windowHeight / 2 && sectionTop + sectionHeight >= windowHeight / 2) {
+      navbarItems.forEach((item, itemIndex) => {
+        if (itemIndex === index) {
+          item.classList.add('active');
+          itemActual = item; // Actualizamos el elemento actual en la variable global
+        } else {
+          item.classList.remove('active');
+        }
+      });
+    }
+  });
 }
   
   // Listener para el evento scroll
@@ -50,9 +49,9 @@ navbarItems.forEach((item, itemIndex) => {
         item.classList.add('active');
       } else {
         item.classList.remove('active');
-        updateNavbarActiveSection(itemActual);
       }
     });
+    updateNavbarActiveSection(); // Llamamos a la función sin parámetros
   });
 });
   
@@ -130,3 +129,10 @@ document.addEventListener('click', (event) => {
     checkInput(textInput, textLabel);
   }
 });
+
+
+window.onbeforeunload = () => {
+  for(const form of document.getElementsByTagName('form')) {
+    form.reset();
+  }
+}
